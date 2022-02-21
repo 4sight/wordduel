f573d7327bfbe1f34116acce43b47c75edca8b56
Greeley Dawson <hgd4th@gmail.com>
2022-02-21 12:10:06 -0500

    randomly pick 7 letters and remove them from letters array

--- src/index.js
+++ src/index.js
@@ -2,87 +2,92 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-    var letters = [];
-    for (let i = 0; i < 9; i++){
-      letters.push({'A': 1})
-    }
-    for (let i = 0; i < 2; i++){
-      letters.push({'B': 3})
-    }
-    for (let i = 0; i < 2; i++){
-      letters.push({'C': 3})
-    }
-    for (let i = 0; i < 4; i++){
-      letters.push({'D': 2})
-    }
-    for (let i = 0; i < 12; i++){
-      letters.push({'E': 1})
-    }
-    for (let i = 0; i < 2; i++){
-      letters.push({'F': 4})
-    }
-    for (let i = 0; i < 3; i++){
-      letters.push({'G': 2})
-    }
-    for (let i = 0; i < 2; i++){
-      letters.push({'H': 4})
-    }
-    for (let i = 0; i < 9; i++){
-      letters.push({'I': 1})
-    }
-    letters.push({'J': 8});
-    letters.push({'K': 5});
-    for (let i = 0; i < 4; i++){
-      letters.push({'L': 1})
-    }
-    for (let i = 0; i < 2; i++){
-      letters.push({'M': 3})
-    }
-    for (let i = 0; i < 6; i++){
-      letters.push({'N': 1})
-    }
-    for (let i = 0; i < 8; i++){
-      letters.push({'O': 1})
-    }
-    for (let i = 0; i < 2; i++){
-      letters.push({'P': 3})
-    }
-    letters.push({'Q': 10});
-    for (let i = 0; i < 6; i++){
-      letters.push({'R': 1})
-    }
-    for (let i = 0; i < 4; i++){
-      letters.push({'S': 1})
-    }
-    for (let i = 0; i < 6; i++){
-      letters.push({'T': 1})
-    }
-    for (let i = 0; i < 4; i++){
-      letters.push({'U': 1})
-    }
-    for (let i = 0; i < 2; i++){
-      letters.push({'V': 4})
-    }
-    for (let i = 0; i < 2; i++){
-      letters.push({'W': 4})
-    }
-    letters.push({'X': 8});
-    for (let i = 0; i < 2; i++){
-      letters.push({'Y': 4})
-    }
-    letters.push({'Z': 10});
-    for (let i = 0; i < 2; i++){
-      letters.push({' ': 0})
-    }
+var letters = [];
+for (let i = 0; i < 9; i++){
+  letters.push({'A': 1})
+}
+for (let i = 0; i < 2; i++){
+  letters.push({'B': 3})
+}
+for (let i = 0; i < 2; i++){
+  letters.push({'C': 3})
+}
+for (let i = 0; i < 4; i++){
+  letters.push({'D': 2})
+}
+for (let i = 0; i < 12; i++){
+  letters.push({'E': 1})
+}
+for (let i = 0; i < 2; i++){
+  letters.push({'F': 4})
+}
+for (let i = 0; i < 3; i++){
+  letters.push({'G': 2})
+}
+for (let i = 0; i < 2; i++){
+  letters.push({'H': 4})
+}
+for (let i = 0; i < 9; i++){
+  letters.push({'I': 1})
+}
+letters.push({'J': 8});
+letters.push({'K': 5});
+for (let i = 0; i < 4; i++){
+  letters.push({'L': 1})
+}
+for (let i = 0; i < 2; i++){
+  letters.push({'M': 3})
+}
+for (let i = 0; i < 6; i++){
+  letters.push({'N': 1})
+}
+for (let i = 0; i < 8; i++){
+  letters.push({'O': 1})
+}
+for (let i = 0; i < 2; i++){
+  letters.push({'P': 3})
+}
+letters.push({'Q': 10});
+for (let i = 0; i < 6; i++){
+  letters.push({'R': 1})
+}
+for (let i = 0; i < 4; i++){
+  letters.push({'S': 1})
+}
+for (let i = 0; i < 6; i++){
+  letters.push({'T': 1})
+}
+for (let i = 0; i < 4; i++){
+  letters.push({'U': 1})
+}
+for (let i = 0; i < 2; i++){
+  letters.push({'V': 4})
+}
+for (let i = 0; i < 2; i++){
+  letters.push({'W': 4})
+}
+letters.push({'X': 8});
+for (let i = 0; i < 2; i++){
+  letters.push({'Y': 4})
+}
+letters.push({'Z': 10});
+for (let i = 0; i < 2; i++){
+  letters.push({' ': 0})
+}
+
+var player1letters = [];
+for (let i = 0; i < 7; i++){
+  let letterIndex = Math.round(Math.random() * letters.length);
+  player1letters.push(letters[letterIndex]);
+  letters.splice(letterIndex, 1);
+}
 
 console.log(letters);
 
 class Square extends React.Component {
   render(){
     return(
-      <button className='square'>
-        {/* TODO */}
-      </button>
+      <button className='square'></button>
     );
   }
 }
